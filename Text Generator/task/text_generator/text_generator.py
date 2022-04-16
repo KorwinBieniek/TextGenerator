a72e9d8ed2d188a799f0deb59105528370386534
Korwin <korwin-bieniek@wp.pl>
2022-04-16 07:06:23 +0200

    Stage 3/6. Counting the number of unique occurrences of the word. Throwing KeyError if a word doesn't exist in the corpus.

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,5 +1,4 @@
 from nltk.tokenize import WhitespaceTokenizer
-from nltk.probability import FreqDist
 from nltk.util import bigrams
 
 tokenizer = WhitespaceTokenizer()
@@ -9,20 +8,29 @@ f = open(filename, "r", encoding="utf-8")
 tokens = tokenizer.tokenize(f.read())
 bigrm = bigrams(tokens)
 bigram_tuple = tuple(bigrm)
-fdist = FreqDist(tokens)
-
-print(f'Number of bigrams: {len(bigram_tuple)}\n')
 
 while True:
-    user_input = input()
-    if user_input == 'exit':
-        break
     try:
-        user_input = int(user_input)
-        print(f'Head: {bigram_tuple[user_input][0]}\t Tail: {bigram_tuple[user_input][1]}')
+        user_input = input()
+        if user_input == 'exit':
+            break
+        print(f'Head: {user_input}')
+        dict_of_words = {}
+        for words in bigram_tuple:
+            if words[0] == user_input:
+                if words[1] in dict_of_words:
+                    dict_of_words[words[1]] += 1
+                else:
+                    dict_of_words[words[1]] = 1
+        if dict_of_words[user_input] == 0:
+            raise KeyError
+        for key, item in dict_of_words.items():
+            print(f'Tail: {key} Count: {item}')
     except TypeError:
         print('Type Error. Please input an integer.')
     except IndexError:
         print('Index Error. Please input an integer that is in the range of the corpus.')
     except ValueError:
         print('ValueError. Please input an integer')
+    except KeyError:
+        print('Key Error. The requested word is not in the model. Please input another word.')
