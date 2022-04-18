f8c6d9d1f680faec32ad672c6e12863064d05020
Korwin <korwin-bieniek@wp.pl>
2022-04-19 00:18:24 +0200

    Stage 6/6. Trained the data on trigrams. The sentences make more sense now.

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,7 +1,7 @@
 import random
 
 from nltk.tokenize import WhitespaceTokenizer
-from nltk.util import bigrams
+from nltk.util import ngrams
 
 
 def open_file():
@@ -9,17 +9,20 @@ def open_file():
     return open(filename, "r", encoding="utf-8")
 
 
-def create_bigrams(f):
+def create_trigrams(f):
     tokenizer = WhitespaceTokenizer()
     text = f.read()
     tokens = tokenizer.tokenize(text)
-    bigrm = bigrams(tokens)
-    return tuple(bigrm)
+    trigrm = ngrams(tokens, 3)
+    trigram = list()
+    for x in trigrm:
+        trigram.append((x[0] + " " + x[1], x[2]))
+    return trigram
 
 
-def count_tail_occurrences(bigram_tuple, word):
+def count_tail_occurrences(trigram_tuple, word):
     dict_of_words = {}
-    for words in bigram_tuple:
+    for words in trigram_tuple:
         if words[0] == word:
             if words[1] in dict_of_words:
                 dict_of_words[words[1]] += 1
@@ -29,32 +32,35 @@ def count_tail_occurrences(bigram_tuple, word):
 
 
 def return_next_word(word):
-    dict_of_occurences = count_tail_occurrences(bigram_tuple, word)
+    dict_of_occurences = count_tail_occurrences(trigram_tuple, word)
     dict_of_occurences = dict(sorted(dict_of_occurences.items(), key=lambda item: item[1], reverse=True))
-    list_of_next_words = list(dict_of_occurences.keys())[:5]
+    list_of_next_words = list(dict_of_occurences.keys())[:3]
     return random.choice(list_of_next_words)
 
-def generate_pseudo_sentence(bigram_tuple):
 
-    first_tuple = random.choice(bigram_tuple)
+def generate_pseudo_sentence(trigram_tuple):
+    first_tuple = random.choice(trigram_tuple)
     while True:
-        if first_tuple[0][0].isupper() and first_tuple[0][-1] not in '.!?':
+        first_token = first_tuple[0].split()
+        if first_token[0][0].isupper() and first_token[0][-1] not in '.!?' and first_token[1][-1] not in '.!?':
             break
         else:
-            first_tuple = random.choice(bigram_tuple)
+            first_tuple = random.choice(trigram_tuple)
     pseudo_sentence = first_tuple[0]
-    next_word = return_next_word(pseudo_sentence)
-    pseudo_sentence += f" {next_word}"
+    last_word = pseudo_sentence.split(" ")[-1]
+    next_word = last_word + " " + return_next_word(pseudo_sentence)
+    pseudo_sentence += f" {next_word.split()[1]}"
     while len(pseudo_sentence.split()) < 5:
-        next_word = return_next_word(next_word)
-        pseudo_sentence += f" {next_word}"
+        next_word = pseudo_sentence.split(" ")[-1] + " " + return_next_word(next_word)
+        pseudo_sentence += f" {next_word.split()[1]}"
     while next_word[-1] not in '.?!':
-        next_word = return_next_word(next_word)
-        pseudo_sentence += f" {next_word}"
+        next_word = pseudo_sentence.split(" ")[-1] + " " + return_next_word(next_word)
+        pseudo_sentence += f" {next_word.split()[1]}"
     print(pseudo_sentence)
 
 
 f = open_file()
-bigram_tuple = create_bigrams(f)
+trigram_tuple = create_trigrams(f)
+first_tuple = random.choice(trigram_tuple)
 for i in range(10):
-    generate_pseudo_sentence(bigram_tuple)
+    generate_pseudo_sentence(trigram_tuple)
